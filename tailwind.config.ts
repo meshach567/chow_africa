a3131c97f669a9ebdacf0c86dcd7a88386b87c2a
meshach567 <arinzemeshach123@gmail.com>
2025-01-27 13:08:57 +0100

    style: update the tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,9 @@
 import withMT from "@material-tailwind/react/utils/withMT";
  
 module.exports = withMT({
-  content: [],
+  content: [
+    "./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {},
   },
